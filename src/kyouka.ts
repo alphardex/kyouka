0cdf3e0f3e48e478c0b4b7adf7a9f272a89a102e
alphardex <2582347430@qq.com>
2020-11-03 12:30:05 +0800

    add 1 util

--- src/kyouka.ts
+++ src/kyouka.ts
@@ -1,32 +1,33 @@
-import arrayToCSV from './arrayToCSV';
-import average from './average';
+import arrayToCSV from "./arrayToCSV";
+import average from "./average";
 import calcDistance from "./calcDistance";
-import CSVToArray from './CSVToArray';
-import CSVToJSON from './CSVToJSON';
-import debounce from './debounce';
-import deepClone from './deepClone';
+import CSVToArray from "./CSVToArray";
+import CSVToJSON from "./CSVToJSON";
+import debounce from "./debounce";
+import deepClone from "./deepClone";
 import disableTouchMove from "./disableTouchMove";
-import formatDuration from './formatDuration';
+import formatDuration from "./formatDuration";
 import getCenterPointPos from "./getCenterPointPos";
 import getMousePos from "./getMousePos";
-import { getScrollPosition, getScrollPositionAsVw } from './getScrollPosition';
-import getTimeDeltaAsSeconds from './getTimeDeltaAsSeconds';
+import { getScrollPosition, getScrollPositionAsVw } from "./getScrollPosition";
+import getTimeDeltaAsSeconds from "./getTimeDeltaAsSeconds";
 import getTimeFromDate from "./getTimeFromDate";
-import getURLParameters from './getURLParameters';
-import initialize2DArray from './initialize2DArray';
-import initializeArrayWithValues from './initializeArrayWithValues';
-import iosInputScrollFix from './iosInputScrollFix';
-import isBottomVisible from './isBottomVisible';
-import isEmpty from './isEmpty';
-import isPhoneNumber from './isPhoneNumber';
-import JSONToCSV from './JSONToCSV';
-import mapKeys from './mapKeys';
-import mapObject from './mapObject';
-import mapValues from './mapValues';
-import omit from './omit';
-import pick from './pick';
-import px2vw from './px2vw';
-import previewImage from './previewImage';
+import getURLParameters from "./getURLParameters";
+import initialize2DArray from "./initialize2DArray";
+import initializeArrayWithValues from "./initializeArrayWithValues";
+import iosInputScrollFix from "./iosInputScrollFix";
+import isBottomVisible from "./isBottomVisible";
+import isEmpty from "./isEmpty";
+import isPhoneNumber from "./isPhoneNumber";
+import JSONToCSV from "./JSONToCSV";
+import mapKeys from "./mapKeys";
+import mapObject from "./mapObject";
+import mapValues from "./mapValues";
+import omit from "./omit";
+import partial from "./partial";
+import pick from "./pick";
+import px2vw from "./px2vw";
+import previewImage from "./previewImage";
 import randomIntArrayInRange from "./randomIntArrayInRange";
 import randomIntegerInRange from "./randomIntegerInRange";
 import randomNumberInRange from "./randomNumberInRange";
@@ -34,52 +35,53 @@ import requestInterval from "./requestInterval";
 import sample from "./sample";
 import shuffle from "./shuffle";
 import sleep from "./sleep";
-import sum from './sum';
-import throttle from './throttle';
-import uniq from './uniq';
+import sum from "./sum";
+import throttle from "./throttle";
+import uniq from "./uniq";
 import vw2px from "./vw2px";
 
 const kyoka = {
-    calcDistance,
-    disableTouchMove,
-    getCenterPointPos,
-    getMousePos,
-    getTimeFromDate,
-    randomIntArrayInRange,
-    randomNumberInRange,
-    randomIntegerInRange,
-    requestInterval,
-    sample,
-    shuffle,
-    sleep,
-    vw2px,
-    sum,
-    formatDuration,
-    getTimeDeltaAsSeconds,
-    isPhoneNumber,
-    isBottomVisible,
-    getURLParameters,
-    average,
-    previewImage,
-    getScrollPosition,
-    getScrollPositionAsVw,
-    px2vw,
-    initialize2DArray,
-    deepClone,
-    initializeArrayWithValues,
-    CSVToArray,
-    arrayToCSV,
-    JSONToCSV,
-    CSVToJSON,
-    uniq,
-    omit,
-    pick,
-    iosInputScrollFix,
-    mapObject,
-    mapKeys,
-    mapValues,
-    isEmpty,
-    debounce,
-    throttle
+  calcDistance,
+  disableTouchMove,
+  getCenterPointPos,
+  getMousePos,
+  getTimeFromDate,
+  randomIntArrayInRange,
+  randomNumberInRange,
+  randomIntegerInRange,
+  requestInterval,
+  sample,
+  shuffle,
+  sleep,
+  vw2px,
+  sum,
+  formatDuration,
+  getTimeDeltaAsSeconds,
+  isPhoneNumber,
+  isBottomVisible,
+  getURLParameters,
+  average,
+  previewImage,
+  getScrollPosition,
+  getScrollPositionAsVw,
+  px2vw,
+  initialize2DArray,
+  deepClone,
+  initializeArrayWithValues,
+  CSVToArray,
+  arrayToCSV,
+  JSONToCSV,
+  CSVToJSON,
+  uniq,
+  omit,
+  pick,
+  iosInputScrollFix,
+  mapObject,
+  mapKeys,
+  mapValues,
+  isEmpty,
+  debounce,
+  throttle,
+  partial,
 };
 export default kyoka;
---
+++ src/partial.ts
@@ -0,0 +1,7 @@
+/**
+ * Creates a function that invokes fn with partials prepended to the arguments it receives.
+ * @param fn
+ * @param partials
+ */
+const partial = (fn: Function, ...partials: any[]) => (...args: any[]) => fn(...partials, ...args);
+export default partial;
