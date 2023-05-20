dbd8fdfa624d9720775a98cf8f19b86b431a62c5
alphardex <2582347430@qq.com>
2023-05-20 18:30:07 +0800

    add type :)

--- src/getURLParameters.ts
+++ src/getURLParameters.ts
@@ -5,8 +5,9 @@
  * @returns {{}}
  */
 const getURLParameters = (url: string): {} =>
+  // @ts-ignore
   (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
-    (a, v) => (
+    (a: any, v: any) => (
       ((a as any)[v.slice(0, v.indexOf("="))] = v.slice(v.indexOf("=") + 1)), a
     ),
     {}
